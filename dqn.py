3615513c91cba41cea8cbf8f9bf6b0109b3b5920
Will Hoback <will.hoback@gmail.com>
2020-02-04 20:26:49 +0000

    install deps and add DQN class

---
+++ dqn.py
@@ -0,0 +1,19 @@
+import gym
+import numpy as np
+import random
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+
+from collections import deque
+
+
+class DQN:
+    def __init__(self, env):
+        self.env = env  # environment for ref shapes in model
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.01
