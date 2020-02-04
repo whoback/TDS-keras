a36b7e50d09c5a81c99aab87da50c55c9b0cd915
Will Hoback <will.hoback@gmail.com>
2020-02-04 20:38:19 +0000

    quick comments

--- dqn.py
+++ dqn.py
@@ -11,9 +11,10 @@ from collections import deque
 class DQN:
     def __init__(self, env):
         self.env = env  # environment for ref shapes in model
-        self.memory = deque(maxlen=2000)
-        self.gamma = 0.95
+        self.memory = deque(maxlen=2000)  # add trials to mem and use rand sample
+        self.gamma = 0.95  # future rewards depreciation factor
+        # take a random action rather than the one we would predict to be the best in that scenario and decay a each successive trial
         self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.01
+        self.learning_rate = 0.01  # std learning rate param
